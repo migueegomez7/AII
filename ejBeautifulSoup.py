b4a199ba4984e9bbb3fb80f702a18d3e27f1bfd0
migueegomez7 <miguelangelgomezx2@gmail.com>
2022-10-03 08:01:41 +0200

    Vamo ahi

--- ejBeautifulSoup.py
+++ ejBeautifulSoup.py
@@ -1,4 +1,5 @@
 from tkinter import messagebox
+from traceback import print_tb
 from bs4 import BeautifulSoup
 import urllib.request
 from tkinter import *
@@ -16,14 +17,26 @@ def extraer_jornadas():
     s = BeautifulSoup(f,"lxml") #lxml is a parser
     l = s.find_all("div", class_= ["cont-modulo","resultados"])
     return l
+'''
+    locales = []
+    visitantes = []
+    for i in range(len(l)):
+        s.find
+'''
+
 
 def almacenar():
     con = connect("ejtkinter.db")
+    con.text_factory = str
     cur = con.cursor()
     cur.execute("DROP TABLE IF EXISTS partidos")
-    cur.execute("CREATE TABLE partidos(ID INTEGER PRIMARY KEY AUTOINCREMENT, JORNADA TEXT NOT NULL, EQUIPO_LOCAL TEXT NOT NULL, EQUIPO_VISITANTE TEXT NOT NULL, GOLES_L INTEGER NOT NULL,GOLES_V INTEGER NOT NULL,LINK TEXT NOT NULL)")
-    
-    
+    cur.execute("CREATE TABLE partidos(ID INTEGER PRIMARY KEY AUTOINCREMENT, JORNADA INTEGER NOT NULL, EQUIPO_LOCAL TEXT NOT NULL, EQUIPO_VISITANTE TEXT NOT NULL, GOLES_L INTEGER NOT NULL,GOLES_V INTEGER NOT NULL,LINK TEXT NOT NULL)")
+    l = extraer_jornadas()
+    for i in l:
+        jornada = i.a['title']
+        match = re.search("([0-9])+",jornada)
+        a = match.group(0)
+        print(a)
     res = cur.execute("SELECT name FROM sqlite_master WHERE name='partidos'")
     if res.fetchone() is not None:
         messagebox.showinfo("Successful Creation","BD creada correctamente")
@@ -32,11 +45,8 @@ def almacenar():
 def listar():
     return "a"
 
-def printea(l):
-    for i in range(len(l)):
-        print("------------- 1 ------- ")
-        print("")
-        print(l[i])
+def printea(s):
+    print("hello there")
 
 def populate(l):
     return "a"
@@ -48,8 +58,8 @@ def ventana_principal():
     b2 =  Button ( top, text = "Listar", relief = RAISED, command = listar)
     b1.pack(side = TOP)
     b2.pack(side = TOP)
-    l = extraer_jornadas()
-    printea(l)
+    s = extraer_jornadas()
+    printea(s)
     top.mainloop()
 
 if __name__ == "__main__":
