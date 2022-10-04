0b239418c1423d528cc8031b3d428fdeaeaed779
migueegomez7 <miguelangelgomezx2@gmail.com>
2022-10-04 17:28:02 +0200

    Falta link para hacer el insert

--- ejBeautifulSoup.py
+++ ejBeautifulSoup.py
@@ -32,11 +32,24 @@ def almacenar():
     cur.execute("DROP TABLE IF EXISTS partidos")
     cur.execute("CREATE TABLE partidos(ID INTEGER PRIMARY KEY AUTOINCREMENT, JORNADA INTEGER NOT NULL, EQUIPO_LOCAL TEXT NOT NULL, EQUIPO_VISITANTE TEXT NOT NULL, GOLES_L INTEGER NOT NULL,GOLES_V INTEGER NOT NULL,LINK TEXT NOT NULL)")
     l = extraer_jornadas()
-    for i in l:
+    for i in l: #Itera sobre jornadas
         jornada = i.a['title']
         match = re.search("([0-9])+",jornada)
         a = match.group(0)
         print(a)
+        tbody = i.find("tbody").contents
+        trs = [x for x in tbody if x != "\n"]
+        for p in trs:
+            local = p.find("td",class_ = "col-equipo-local").span.string
+            visitante = p.find("td",class_ = "col-equipo-visitante").span.next_sibling.next_sibling.string
+            resultado = p.find("a", class_ = "resultado").string
+            stringa = "sadasdas 1234 - 567 - 9084 \n" #peta si le meto más números , pq?
+            goles_l = re.findall("([0-9])+",resultado)[0]
+            goles_v = re.findall("([0-9])+",resultado)[1]
+            link = p.find("td", class_ = "col-resultado").a["href"]
+
+                
+        cur.execute("INSERT INTO partidos VALUES (?,?,?,?,?,?)",(a,local,visitante,golesl,golesv,link))
     res = cur.execute("SELECT name FROM sqlite_master WHERE name='partidos'")
     if res.fetchone() is not None:
         messagebox.showinfo("Successful Creation","BD creada correctamente")
